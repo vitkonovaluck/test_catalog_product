0c7b133ef7b364a57ec4cc7790abd9b164c02680
DJey <vkonovaluck@gmail.com>
2025-09-17 09:17:24 +0300

    fix: timezone

--- public/index.php
+++ public/index.php
@@ -8,7 +8,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 // Встановити часовий пояс
-date_default_timezone_set('Europe/Kiev');
+date_default_timezone_set('Europe/Kyiv');
 
 // Встановити кодування
 mb_internal_encoding('UTF-8');
